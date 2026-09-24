d93f43d3406a971f8890a6b8ee9b82f0ce30bf86
Felipe <felipeshimizu5@gmail.com>
2026-09-24 20:59:13 -0300

    Configurando o projeto

---
+++ src/main/java/com/liora/creditagent/LioraCreditAgentApplication.java
@@ -0,0 +1,13 @@
+package com.liora.creditagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LioraCreditAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LioraCreditAgentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/liora/creditagent/LioraCreditAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liora.creditagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LioraCreditAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
